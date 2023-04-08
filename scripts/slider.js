b5e75b4ff4b2d28ee0cb883b9cb66cce52b2f289
PArrotGames (Andrey Sergeev) <parrotgames44@gmail.com>
2023-04-09 01:46:09 +0700

    done adaptive

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
     $('.shop__slider').slick(
         {
-            slidesToShow: 3,
+            slidesToShow: 1,
             touchThreshold: 20,
-            responsive: [
-                {
-                    breakpoint: 1100,
-                    settings: {
-                        slidesToShow: 2
-                    },
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1
-                    },
-                }
-            ]
+            // responsive: [
+            //     {
+            //         breakpoint: 1100,
+            //         settings: {
+            //             slidesToShow: 2
+            //         },
+            //     },
+            //     {
+            //         breakpoint: 768,
+            //         settings: {
+            //             slidesToShow: 1
+            //         },
+            //     }
+            // ]
         }
     );
 
